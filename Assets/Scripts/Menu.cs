979ca8a329adc06827407c8afe01914c95d80adb
tienloc1234 <tienloc2508@gmail.com>
2026-06-19 14:48:42 +0700

    Add sound toggle button to menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Game");
     }
+
     public void QuitGame()
-        {
-            Application.Quit();
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
